70c441318601a858c4f6a0f6e2f1b9f130d7963c
aaronbeecham <39786760+aaronbeecham@users.noreply.github.com>
2024-11-20 14:06:59 +1100

    updates

---
+++ companyLinks.js
@@ -0,0 +1,220 @@
+// companyLinks.js
+
+const companyLinks = {
+    "Family Services Australia (FSA)": {
+        "itGlueId": "8260864",
+        "credentialsId": "8576248",
+        "localAdminId": "24481000"
+    },
+    "Peoplecare": {
+        "itGlueId": "5429844",
+        "credentialsId": "15552914",
+        "localAdminId": "25303391"
+    },
+    "State Sport Centres Trust (SSCT)": {
+        "itGlueId": "8261103",
+        "credentialsId": "10494182",
+        "localAdminId": "10972458"
+    },
+    "Lubrication Engineers": {
+        "itGlueId": "8260966",
+        "credentialsId": "2812021",
+        "localAdminId": "4515777"
+    },
+    "Heard McEwan Pty Limited": {
+        "itGlueId": "8260899",
+        "credentialsId": "6090767",
+        "localAdminId": "6078049"
+    },
+    "ECTARC": {
+        "itGlueId": "8260856",
+        "credentialsId": "26041223",
+        "localAdminId": "25847056"
+    },
+    "Aquilius Investment Partners Pte. Ltd.": {
+        "itGlueId": "8260774",
+        "credentialsId": "17559312",
+        "localAdminId": "17559429"
+    },
+    "Polyflor": {
+        "itGlueId": "8261037",
+        "credentialsId": "10096962",
+        "localAdminId": "15045798"
+    },
+    "Chamberlains": {
+        "itGlueId": "8260821",
+        "credentialsId": "23003598",
+        "localAdminId": "4515702"
+    },
+    "Axis Plumbing": {
+        "itGlueId": "4585191",
+        "credentialsId": "13763560",
+        "localAdminId": "17144505"
+    },
+    "AWN Food & Fibre Holdings Pty Ltd": {
+        "itGlueId": "8260787",
+        "credentialsId": "18694396",
+        "localAdminId": "20819327"
+    },
+    "Chartertech Pty Ltd": {
+        "itGlueId": "8260823",
+        "credentialsId": "14265908",
+        "localAdminId": "20114900"
+    },
+    "Evolve Housing": {
+        "itGlueId": "4842737",
+        "credentialsId": "22010818",
+        "localAdminId": "14628112"
+    },
+    "Girl Guides NSW, ACT & NT": {
+        "itGlueId": "8260882",
+        "credentialsId": "20606178",
+        "localAdminId": ""
+    },
+    "Endotherapeutics Pty Ltd": {
+        "itGlueId": "8260859",
+        "credentialsId": "23434860",
+        "localAdminId": "22099378"
+    },
+    "Advanced Constructions": {
+        "itGlueId": "8260765",
+        "credentialsId": "18342882",
+        "localAdminId": "17144505"
+    },
+    "Centurion Healthcare": {
+        "itGlueId": "8260819",
+        "credentialsId": "19132422",
+        "localAdminId": "24621473"
+    },
+    "Curijo Pty Ltd": {
+        "itGlueId": "8260839",
+        "credentialsId": "14277889",
+        "localAdminId": "15820693"
+    },
+    "My Gateway": {
+        "itGlueId": "8261000",
+        "credentialsId": "2291285",
+        "localAdminId": "4515786"
+    },
+    "Domaine Wine Shippers (DWS)": {
+        "itGlueId": "8260846",
+        "credentialsId": "27996400",
+        "localAdminId": "19777052"
+    },
+    "align.me": {
+        "itGlueId": "4314127",
+        "credentialsId": "17451897",
+        "localAdminId": "9732988"
+    },
+    "CleanSpace Technology Pty Ltd": {
+        "itGlueId": "8260753",
+        "credentialsId": "16717196",
+        "localAdminId": "7488319"
+    },
+    "Care Pharmaceuticals": {
+        "itGlueId": "8260810",
+        "credentialsId": "16884291",
+        "localAdminId": "6375172"
+    },
+    "Nugan Estate Wines": {
+        "itGlueId": "3614543",
+        "credentialsId": "7001345",
+        "localAdminId": "19082437"
+    },
+    "Mini Tankers Australia Pty Ltd (RFS)": {
+        "itGlueId": "8260990",
+        "credentialsId": "23630488",
+        "localAdminId": "24408470"
+    },
+    "Australian Indigenous Education Foundation (AIEF)": {
+        "itGlueId": "8260782",
+        "credentialsId": "16716437",
+        "localAdminId": "24325209"
+    },
+    "RSL National": {
+        "itGlueId": "8261081",
+        "credentialsId": "13762888",
+        "localAdminId": "24325209"
+    },
+    "Briemar Nominees Pty Ltd": {
+        "itGlueId": "8260810",
+        "credentialsId": "16884291",
+        "localAdminId": "6375172",
+    },
+    "ROC Partners Pty Limited": {
+        "itGlueId": "3010977",
+        "credentialsId": "5617401",
+        "localAdminId": "5617396",
+        "useGreenlightUrl": true
+    },
+    "Infrastructure Sustainability Council (ISCA)": {
+        "itGlueId": "6174644",
+        "credentialsId": "19652915",
+        "localAdminId": "17750338",
+    },
+    "Guidolin Agrimac": {
+        "itGlueId": "8260895",
+        "credentialsId": "17046512",
+        "localAdminId": "17032597",
+    },
+    "Australian Amalgamated Terminals (AAT)": {
+        "itGlueId": "8261173",
+        "credentialsId": "21242437",
+        "localAdminId": "21242437",
+    },
+    "Valiant Commercial Furniture": {
+        "itGlueId": "8261136",
+        "credentialsId": "7571715",
+        "localAdminId": "7488319",
+    },
+    "Back to Back Financial Planners": {
+        "itGlueId": "8260789",
+        "credentialsId": "14265815",
+        "localAdminId": "16816209",
+    },
+    "IAH Sales Pty Ltd": {
+        "itGlueId": "8260909",
+        "credentialsId": "22578456",
+        "localAdminId": "25610138",
+    },
+    "Mayo Hardware": {
+        "itGlueId": "8260981",
+        "credentialsId": "3473633",
+        "localAdminId": "4515782",
+    },
+    "Procurement Australia": {
+        "itGlueId": "8261043",
+        "credentialsId": "7082834",
+        "localAdminId": "18916698",
+    },
+    "Splosh Australia": {
+        "itGlueId": "8261100",
+        "credentialsId": "27648201",
+        "localAdminId": "27417205",
+    },
+    "Air Affairs Australia": {
+        "itGlueId": "8260767",
+        "credentialsId": "23117487",
+        "localAdminId": "",
+    },
+    "Macedonian Welfare Association": {
+        "itGlueId": "8260970",
+        "credentialsId": "26043097",
+        "localAdminId": "25667020",
+    },
+    "VFX Print Group": {
+        "itGlueId": "8261137",
+        "credentialsId": "3663335",
+        "localAdminId": "4517162",
+    },
+    "Cater & Blumer": {
+        "itGlueId": "8260815",
+        "credentialsId": "26927946",
+        "localAdminId": "27166668",
+    },
+    "Jackson Ranch": {
+        "itGlueId": "8260790",
+        "credentialsId": "28603048",
+        "localAdminId": "4632827",
+    },
+};
